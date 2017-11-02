8882b1e5be1f025731269a1794060a9b0201a399
breckinridge <BreckStodghill@Breckinridges-MacBook-Pro.local>
2017-11-02 11:43:32 -0500

    Added some urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.login, name='login'),
+    url(r'^$signup/', views.sign_up, name='sign_up')
+    # log in screen
+    # sign up screen
+    # dashboard for providers -> has a request Function
+        # display prescriptions -> after a request, allow for a new script to be added
+    # prescriptions page -> prescriptions a doctor has written
+
+    # dashboard for patients -> has historical prescriptions displayed
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Optrak")
+def login(request):
+    return HttpResponse("Login to Optrak")
+
+
+def sign_up(request):
+    return HttpResponse("Sign up for Optrak")
