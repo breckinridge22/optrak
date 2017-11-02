5bfe5efb3446413e0e517625f79f1e1d915b36f2
breckinridge <BreckStodghill@Breckinridges-MacBook-Pro.local>
2017-11-02 00:47:38 -0500

    Added a basic view, a postgres db with a role, and some models

---
+++ webapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Provider(models.Model):
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+
+class Prescription(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True)
+    drugId = models.IntegerField(default = 0)
+    dosage = models.IntegerField(default = 0)
+    dateWritten = models.DateField(auto_now_add=False, null=True)
+    dateFilled = models.DateField(auto_now_add=False, null=True)
+
+class Request(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True)
+
+class Response(models.Model):
+     STATUS_CHOICES = (('0', 'Failure'), ('1', 'Success'))
+     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
+     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True)
+     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
