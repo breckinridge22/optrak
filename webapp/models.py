21f4a3515c923ab5272d802c2f4eae0869420413
Dana Zhang <peng.zhang@vanderbilt.edu>
2017-11-17 18:01:07 -0600

    Merge from master branch

--- webapp/models.py
+++ webapp/models.py
@@ -2,21 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+# when a new provider is created -- create a key pair and store the id on the chain
 class Provider(models.Model):
-    first_name = models.CharField(max_length = 20)
-    last_name = models.CharField(max_length = 20)
+    # this will likely just be a hash
+    provider_id = models.IntegerField(default = 0)
 
+# when a new patient is created -- create a key pair and store the id on the chain
 class Patient(models.Model):
-    first_name = models.CharField(max_length = 20)
-    last_name = models.CharField(max_length = 20)
+    # this will likely just be a hash
+    patient_id = models.IntegerField(default = 0)
 
 class Prescription(models.Model):
-    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True)
-    drugId = models.IntegerField(default = 0)
-    dosage = models.IntegerField(default = 0)
-    dateWritten = models.DateField(auto_now_add=False, null=True)
-    dateFilled = models.DateField(auto_now_add=False, null=True)
+    prescription_id = models.IntegerField(default = 0)
+    #provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
+    #patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True)
+    #drugId = models.IntegerField(default = 0)
+    #dosage = models.IntegerField(default = 0)
+    #dateWritten = models.DateField(auto_now_add=False, null=True)
+    #dateFilled = models.DateField(auto_now_add=False, null=True)
 
 class Request(models.Model):
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
@@ -27,3 +30,4 @@ class Response(models.Model):
      provider = models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
      patient = models.ForeignKey(Patient, on_delete=models.CASCADE, blank=True, null=True)
      status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+     # how do we implement the API access key
--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 
-
 def login(request):
     return HttpResponse("Login to Optrak")
 
-
 def sign_up(request):
+    # store their public key on the blockchain
     return HttpResponse("Sign up for Optrak")
+
+def providerDashboard(request):
+    return HttpResponse("Provider Dashboard")
+
+def patientDashboard(request):
+    return HttpResponse("Patient Dashboard")
+
+def patientPrescriptions(request):
+    return HttpResponse("Patient Prescriptions")
+
+def providerPrescriptions(request):
+    return HttpResponse("Provider Prescriptions")
