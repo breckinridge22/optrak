d5e4a9d10d613009672060168a60d4a7503680c4
Dana Zhang <peng.zhang@vanderbilt.edu>
2017-12-11 17:05:21 -0600

    add example login

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,42 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from webapp.forms import UserForm
+
+# Extra Imports for the Login and Logout Capabilities
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
 
 def login(request):
-    return HttpResponse("Login to Optrak")
+    if request.method == 'POST':
+	    # First get the username and password supplied
+	    username = request.POST.get('username')
+	    password = request.POST.get('password')
+
+	    # Django's built-in authentication function:
+	    user = authenticate(username=username, password=password)
+
+	    # If we have a user
+	    if user:
+	        #Check it the account is active
+	        if user.is_active:
+	            # Log the user in.
+	            login(request,user)
+	            # Send the user back to some page.
+	            # In this case their homepage.
+	            return HttpResponseRedirect(reverse('index'))
+	        else:
+	            # If account is not active:
+	            return HttpResponse("Your account is not active.")
+	    else:
+	        print("Someone tried to login and failed.")
+	        print("They used username: {} and password: {}".format(username,password))
+	        return HttpResponse("Invalid login details supplied.")
+
+    else:
+    	return render(request, "index.html")
 
 def sign_up(request):
     # store their public key on the blockchain
