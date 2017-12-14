5e970762393430fa8971dfafcdf0cffe92430fbf
Dana Zhang <peng.zhang@vanderbilt.edu>
2017-12-14 13:13:13 -0600

    login function added

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login, name='login'),
+    url(r'^$', views.index, name='index'),
     url(r'^signup/$', views.sign_up, name='sign_up'),
     url(r'^provider/dashboard/$', views.providerDashboard, name = 'providerDashboard'),
     url(r'^provider/prescriptions/$', views.providerPrescriptions, name = 'providerPrescriptions'),
--- webapp/views.py
+++ webapp/views.py
@@ -9,47 +9,83 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
 
-def login(request):
+def index(request):
     if request.method == 'POST':
-	    # First get the username and password supplied
-	    username = request.POST.get('username')
-	    password = request.POST.get('password')
+    	if 'login' in request.POST:
+    		return handleLogin(request)
+    	elif 'register' in request.POST:
+    		return handleRegister(request)
+    else:
+    	return render(request, 'index.html', {})
 
-	    # Django's built-in authentication function:
-	    user = authenticate(username=username, password=password)
+def handleLogin(request):
+    # First get the username and password supplied
+    username = request.POST.get('username')
+    password = request.POST.get('password')
 
-	    # If we have a user
-	    if user:
-	        #Check it the account is active
-	        if user.is_active:
-	            # Log the user in.
-	            login(request,user)
-	            # Send the user back to some page.
-	            # In this case their homepage.
-	            return HttpResponseRedirect(reverse('index'))
-	        else:
-	            # If account is not active:
-	            return HttpResponse("Your account is not active.")
-	    else:
-	        print("Someone tried to login and failed.")
-	        print("They used username: {} and password: {}".format(username,password))
-	        return HttpResponse("Invalid login details supplied.")
+    # Django's built-in authentication function:
+    user = authenticate(username=username, password=password)
+
+    # If we have a user
+    if user:
+        #Check it the account is active
+        if user.is_active:
+            # Log the user in.
+            login(request,user)
+            # Send the user back to some page.
+            # In this case their homepage.
+            return HttpResponseRedirect(reverse('webapp:index'))
+        else:
+            # If account is not active:
+            return HttpResponse('Your account is not active.')
+    else:
+        print('Someone tried to login and failed.')
+        print('They used username: {} and password: {}'.format(username,password))
+        return HttpResponse('Invalid login details supplied.')
+
+def handleRegister(request):
+    registered = False
+
+    # Get info from 'both' forms
+    # It appears as one form to the user on the .html page
+    user_form = UserForm(data=request.POST)
+
+    # Check to see both forms are valid
+    if user_form.is_valid():
+
+        # Save User Form to Database
+        user = user_form.save()
+
+        # Hash the password
+        user.set_password(user.password)
+
+        # Update with Hashed password
+        user.save()
+        print('----------------------------------------registering-----------' + user.password)
+
+        # Registration Successful!
+        registered = True
+        return render(request, 'index.html', {'user_form': user_form, 'registered': registered})
 
     else:
-    	return render(request, "index.html")
+        # One of the forms was invalid if this else gets called.
+        print(user_form.errors)
+        return HttpResponse('Your account is not registered....')
+
+
 
 def sign_up(request):
     # store their public key on the blockchain
-    return HttpResponse("Sign up for Optrak")
+    return HttpResponse('Sign up for Optrak')
 
 def providerDashboard(request):
-    return HttpResponse("Provider Dashboard")
+    return HttpResponse('Provider Dashboard')
 
 def patientDashboard(request):
-    return HttpResponse("Patient Dashboard")
+    return HttpResponse('Patient Dashboard')
 
 def patientPrescriptions(request):
-    return HttpResponse("Patient Prescriptions")
+    return HttpResponse('Patient Prescriptions')
 
 def providerPrescriptions(request):
-    return HttpResponse("Provider Prescriptions")
+    return HttpResponse('Provider Prescriptions')
